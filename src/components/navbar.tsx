0263fe350ecfb310b9b9f6be308c5225749af795
sgtclickclack <julian.g.roberts@gmail.com>
2025-11-20 23:36:41 +1000

    style(ui): remove text from navbar logo and center hero logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -77,9 +77,8 @@ export default function Navbar() {
             <img 
               src={logo} 
               alt="Snipshift Logo" 
-              className="h-10 w-auto mr-3" 
+              className="h-10 w-auto" 
             />
-            <span className="text-xl font-bold text-white">Snipshift</span>
           </button>
           
           <div className="flex items-center space-x-4">
