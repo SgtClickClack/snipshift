2337a30d02ccfbb1367b367dade7043d21ac9fa6
sgtclickclack <julian.g.roberts@gmail.com>
2025-12-04 09:19:26 +1000

    fix: update tailwind darkMode config to fix light mode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import tailwindcssAnimate from 'tailwindcss-animate';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
